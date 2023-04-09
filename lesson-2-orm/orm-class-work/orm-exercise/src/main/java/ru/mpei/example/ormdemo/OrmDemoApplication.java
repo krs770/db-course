4f2ea9716c4098a94a59f833fdd8be982ad008e1
Denis Bondar <denis_bondar_97@mail.ru>
2023-04-09 13:28:21 +0300

    Feat: added EM example

--- lesson-2-orm/orm-class-work/orm-exercise/src/main/java/ru/mpei/example/ormdemo/OrmDemoApplication.java
+++ lesson-2-orm/orm-class-work/orm-exercise/src/main/java/ru/mpei/example/ormdemo/OrmDemoApplication.java
@@ -1,17 +1,46 @@
 package ru.mpei.example.ormdemo;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import java.util.List;
 import org.h2.tools.Console;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.sql.SQLException;
+import org.springframework.context.ConfigurableApplicationContext;
+import ru.mpei.example.ormdemo.models.Avatar;
+import ru.mpei.example.ormdemo.models.EMail;
+import ru.mpei.example.ormdemo.models.Student;
 
 @SpringBootApplication
 public class OrmDemoApplication {
 
-	public static void main(String[] args) throws SQLException {
-		SpringApplication.run(OrmDemoApplication.class, args);
+    public static void main(String[] args) throws SQLException {
+        ConfigurableApplicationContext applicationContext =
+            SpringApplication.run(OrmDemoApplication.class, args);
+		EntityManagerFactory factory = applicationContext.getBean(EntityManagerFactory.class);
+		EntityManager entityManager = factory.createEntityManager();
+
+		Student student = new Student();
+		student.setName("Sergey");
+		Avatar avatar = new Avatar();
+		avatar.setPhotoUrl("my_photo");
+		avatar.setStudent(student);
+
+		EMail eMail = new EMail();
+		eMail.setEmail("student_email");
+		List<EMail> eMail1 = List.of(eMail);
+
+		student.setEmails(eMail1);
+		student.setSomeAvatar(avatar);
+
+		entityManager.getTransaction().begin();
+		entityManager.persist(student);
+
+		entityManager.getTransaction().commit();
+
 		Console.main(args);
-	}
+    }
 
 }
