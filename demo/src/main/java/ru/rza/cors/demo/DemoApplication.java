326c8ebedb4da55c7548daf3c7ec76c7dc30babd
Denis Bondar <denis_bondar_97@mail.ru>
2023-04-14 10:19:49 +0300

    Feat: added liquibase foreign key example

--- demo/src/main/java/ru/rza/cors/demo/DemoApplication.java
+++ demo/src/main/java/ru/rza/cors/demo/DemoApplication.java
@@ -2,11 +2,7 @@ package ru.rza.cors.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @EnableJpaRepositories
@@ -15,16 +11,5 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
-
-
-/*    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedMethods("GET", "");
-            }
-        };
-    }*/
 
 }
